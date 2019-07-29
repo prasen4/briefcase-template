506597405d61272472d1c376a2a07d5728b36f51
prasen4 <46898167+prasen4@users.noreply.github.com>
2019-07-28 20:55:27 -0500

    Update app.py

--- {{ cookiecutter.app_name }}/{{ cookiecutter.app_name }}/app.py
+++ {{ cookiecutter.app_name }}/{{ cookiecutter.app_name }}/app.py
@@ -21,25 +21,61 @@ class {{ app_class_name }}(toga.App):
 
 def main():
     return {{ app_class_name }}('{{ cookiecutter.formal_name }}', '{{ cookiecutter.bundle }}.{{ cookiecutter.app_name }}')
-{% elif cookiecutter.gui_framework == 'PySide2' %}import sys
-from PySide2 import QtWidgets
+{% elif cookiecutter.gui_framework == 'PySide2' %}
+
+import cv2
+from gaze_tracking import GazeTracking
+
+gaze = GazeTracking()
+webcam = cv2.VideoCapture(0)
+count=1;
+left_pupil_comp=0
+right_pupil_comp=0
+
+while True:
+
+    _, frame = webcam.read()
 
 
-class {{ app_class_name }}(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.init_ui()
+    gaze.refresh(frame)
 
-    def init_ui(self):
-        self.setWindowTitle('{{ cookiecutter.app_name }}')
-        self.show()
+    frame = gaze.annotated_frame()
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    main_window = {{ app_class_name }}()
-    sys.exit(app.exec_())
-{% else -%}
-def main():
-    # This should start and launch your app!
-    pass
-{% endif -%}
+    left_pupil = gaze.pupil_left_coords()
+    right_pupil = gaze.pupil_right_coords()
+    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Count: " + str(count), (90,200), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+
+    left_pupil_x = gaze.pupil_left_xcoord()
+    left_pupil_int = str(left_pupil_x)
+
+    if left_pupil_int == "None":
+        left_pupilx=0
+    else:
+        left_pupilx=int(left_pupil_int )
+
+    left_pupil_comp += left_pupilx
+    left_pupil_average = left_pupil_comp/count
+
+    right_pupil_x=gaze.pupil_right_xcoord()
+    right_pupil_int = str(right_pupil_x)
+
+    if right_pupil_int == "None":
+        right_pupilx = 0
+    else:
+        right_pupilx = int(right_pupil_int)
+
+    right_pupil_comp += right_pupilx
+    right_pupil_average = right_pupil_comp/count
+
+    cv2.putText(frame, "Left Pupil Average: " + str(left_pupil_average), (90, 235), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Right Pupil Average: " + str(right_pupil_average), (90, 270), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+
+    count +=1;
+
+
+    cv2.imshow("DemoPS", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
